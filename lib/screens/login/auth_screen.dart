4b793bfc2eb02ad136aa74ced99bf44974dbe167
halurabiyyu <halurabiyyu@gmail.com>
2024-11-04 11:26:53 +0700

    fix : fix bug in login

--- lib/screens/login/auth_screen.dart
+++ lib/screens/login/auth_screen.dart
@@ -91,155 +91,170 @@ class AuthScreenState extends State<AuthScreen> {
     }
 
     setState(() {
-      emailError = emailController.text.isEmpty ? 'Please enter your email' : null;
-      passwordError = passwordController.text.isEmpty ? 'Please enter your password' : null;
+      isLoading = true;
+      errorMessage = null;
     });
 
-    //   if (emailError == null && passwordError == null) {
-    //     Navigator.pushReplacementNamed(context, '/main');
-    //   }
-    // }
-
-    void _navigateWithoutTransition(BuildContext context, String routeName) {
+    try {
+      UserCredential userCredential = await _authService.signInWithEmailPassword(
+        emailController.text,
+        passwordController.text,
+      );
+      String? idToken = await userCredential.user?.getIdToken();
+      if (idToken != null) await _authService.callLoginApi(emailController.text, passwordController.text, idToken);
+      setState(() => isSignIn = true);
       Navigator.pushReplacement(
         context,
-        PageRouteBuilder(
-          pageBuilder: (context, animation1, animation2) {
-            return routeName == '/signin' ? AuthScreen(isSignIn: true) : AuthScreen(isSignIn: false);
-          },
-          transitionDuration: Duration.zero,
-          reverseTransitionDuration: Duration.zero,
-        ),
+        MaterialPageRoute(builder: (context) => const MainScreen()),
       );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        if (e.code == 'user-not-found') {
+          emailError = 'No user found for that email.';
+        } else if (e.code == 'wrong-password') {
+          passwordError = 'Wrong password provided for that user.';
+        } else {
+          errorMessage = 'An error occurred. Please try again.';
+        }
+      });
+    } finally {
+      setState(() => isLoading = false);
     }
+  }
 
-    Widget _buildInputField(String labelText, String hintText, bool isPassword, TextEditingController controller, String? error) {
-      return InputField(
-        labelText: labelText,
-        hintText: hintText,
-        isPassword: isPassword,
-        controller: controller,
-        error: error,
-        maxLines: isPassword ? 1 : null, // Ensure maxLines is 1 for password fields
-        onChanged: (value) {
-          setState(() {
-            error = value.isEmpty ? 'Please enter your ${labelText.toLowerCase()}' : null;
-          });
+  void _navigateWithoutTransition(BuildContext context, String routeName) {
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation1, animation2) {
+          return routeName == '/signin' ? AuthScreen(isSignIn: true) : AuthScreen(isSignIn: false);
         },
-      );
-    }
+        transitionDuration: Duration.zero,
+        reverseTransitionDuration: Duration.zero,
+      ),
+    );
+  }
 
-    Widget _buildDivider() {
-      return const Row(
-        children: [
-          Expanded(child: Divider(color: Colors.grey)),
-          SizedBox(width: 10),
-          Text('atau', style: TextStyle(color: Colors.grey, fontSize: 14, fontWeight: FontWeight.w400)),
-          SizedBox(width: 10),
-          Expanded(child: Divider(color: Colors.grey)),
-        ],
-      );
-    }
+  Widget _buildInputField(String labelText, String hintText, bool isPassword, TextEditingController controller, String? error) {
+    return InputField(
+      labelText: labelText,
+      hintText: hintText,
+      isPassword: isPassword,
+      controller: controller,
+      error: error,
+      maxLines: isPassword ? 1 : null, // Ensure maxLines is 1 for password fields
+      onChanged: (value) {
+        setState(() {
+          error = value.isEmpty ? 'Please enter your ${labelText.toLowerCase()}' : null;
+        });
+      },
+    );
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      final screenSize = MediaQuery.of(context).size;
-      final title = widget.isSignIn ? 'Masuk' : 'Daftar';
-      final welcomeText = widget.isSignIn ? 'Selamat datang kembali!' : 'Daftar';
-      final instructionText = widget.isSignIn ? 'Silakan masuk ke akun Anda' : 'Buat Akun Anda';
+  Widget _buildDivider() {
+    return const Row(
+      children: [
+        Expanded(child: Divider(color: Colors.grey)),
+        SizedBox(width: 10),
+        Text('atau', style: TextStyle(color: Colors.grey, fontSize: 14, fontWeight: FontWeight.w400)),
+        SizedBox(width: 10),
+        Expanded(child: Divider(color: Colors.grey)),
+      ],
+    );
+  }
 
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(title, style: const TextStyle(color: Colors.white, fontSize: 24)),
-          centerTitle: true,
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-            onPressed: () => Navigator.pop(context),
-          ),
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final title = widget.isSignIn ? 'Masuk' : 'Daftar';
+    final welcomeText = widget.isSignIn ? 'Selamat datang kembali!' : 'Daftar';
+    final instructionText = widget.isSignIn ? 'Silakan masuk ke akun Anda' : 'Buat Akun Anda';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title, style: const TextStyle(color: Colors.white, fontSize: 24)),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
         ),
-        body: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: screenSize.height - kToolbarHeight - MediaQuery.of(context).padding.top,
-            ),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/logo_blue.png', height: screenSize.height * 0.17),
-                    SizedBox(height: screenSize.height * 0.01),
-                    Text(welcomeText, style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 26, fontWeight: FontWeight.w600)),
-                    SizedBox(height: screenSize.height * 0.01),
-                    Text(instructionText, style: const TextStyle(color: Colors.black, fontSize: 16)),
-                    SizedBox(height: screenSize.height * 0.01),
-                    _buildInputField('Email', 'Email', false, emailController, emailError),
-                    SizedBox(height: screenSize.height * 0.01),
-                    _buildInputField('Password', 'Kata Sandi', true, passwordController, passwordError),
-                    if (!widget.isSignIn) ...[
-                      SizedBox(height: screenSize.height * 0.02), // Additional height for sign-up
-                    ],
-                    if (widget.isSignIn) ...[
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const ForgotPasswordScreen()),
-                            );
-                          },
-                          child: const Text('Lupa Kata Sandi?'),
-                        ),
+      ),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: screenSize.height - kToolbarHeight - MediaQuery.of(context).padding.top,
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/logo_blue.png', height: screenSize.height * 0.17),
+                  SizedBox(height: screenSize.height * 0.01),
+                  Text(welcomeText, style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 26, fontWeight: FontWeight.w600)),
+                  SizedBox(height: screenSize.height * 0.01),
+                  Text(instructionText, style: const TextStyle(color: Colors.black, fontSize: 16)),
+                  SizedBox(height: screenSize.height * 0.01),
+                  _buildInputField('Email', 'Email', false, emailController, emailError),
+                  SizedBox(height: screenSize.height * 0.01),
+                  _buildInputField('Password', 'Kata Sandi', true, passwordController, passwordError),
+                  if (!widget.isSignIn) ...[
+                    SizedBox(height: screenSize.height * 0.02), // Additional height for sign-up
+                  ],
+                  if (widget.isSignIn) ...[
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const ForgotPasswordScreen()),
+                          );
+                        },
+                        child: const Text('Lupa Kata Sandi?'),
                       ),
-                    ],
-                    SizedBox(height: screenSize.height * 0.01),
-                    CustomButton(
-                      text: title,
-                      widthFactor: 1.0,
-                      onPressed: () async {
-                        if (widget.isSignIn) {
-                          await signInWithEmailPassword();
-                        } else {
-                          await signUpWithEmailPassword();
-                        }
-                      },
-                    ),
-                    SizedBox(height: screenSize.height * 0.01),
-                    _buildDivider(),
-                    SizedBox(height: screenSize.height * 0.01),
-                    const CustomButton(
-                      widthFactor: 1.0,
-                      onPressed: signInWithGoogle,
-                      color: Colors.white,
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      svgIconPath: 'assets/icon/google.svg',
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(widget.isSignIn ? 'Belum punya akun?' : 'Sudah punya akun?', style: const TextStyle(color: Colors.black, fontSize: 16)),
-                        TextButton(
-                          onPressed: () => _navigateWithoutTransition(context, widget.isSignIn ? '/signup' : '/signin'),
-                          child: Text(widget.isSignIn ? 'Daftar' : 'Masuk', style: const TextStyle(color: Colors.blue, fontSize: 16)),
-                        ),
-                      ],
                     ),
                   ],
-                ),
+                  SizedBox(height: screenSize.height * 0.01),
+                  CustomButton(
+                    text: title,
+                    widthFactor: 1.0,
+                    onPressed: () async {
+                      if (widget.isSignIn) {
+                        await signInWithEmailPassword();
+                      } else {
+                        await signUpWithEmailPassword();
+                      }
+                    },
+                  ),
+                  SizedBox(height: screenSize.height * 0.01),
+                  _buildDivider(),
+                  SizedBox(height: screenSize.height * 0.01),
+                  const CustomButton(
+                    widthFactor: 1.0,
+                    onPressed: signInWithGoogle,
+                    color: Colors.white,
+                    borderSide: BorderSide(color: Colors.black, width: 2),
+                    svgIconPath: 'assets/icon/google.svg',
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(widget.isSignIn ? 'Belum punya akun?' : 'Sudah punya akun?', style: const TextStyle(color: Colors.black, fontSize: 16)),
+                      TextButton(
+                        onPressed: () => _navigateWithoutTransition(context, widget.isSignIn ? '/signup' : '/signin'),
+                        child: Text(widget.isSignIn ? 'Daftar' : 'Masuk', style: const TextStyle(color: Colors.blue, fontSize: 16)),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
         ),
-      );
-    }
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+      ),
+    );
   }
 }
